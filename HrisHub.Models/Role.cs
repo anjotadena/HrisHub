c415e76ede98dc824bab8abe0803ea7e96fac091
Anjo Tadena <tadena.anjo@gmail.com>
2023-02-19 07:56:38 +0800

    Initial commit

---
+++ HrisHub.Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrisHub.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(300)]
+        public string Description { get; set; } = string.Empty;
+
+        public ICollection<User>? Users { get; set; }
+    }
+}
---
+++ HrisHub.Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrisHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Email is a required field!")]
+        public string Email { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Password is a required field!")]
+        public string Password { get; set; } = string.Empty;
+
+        public int RoleId { get; set; }
+
+        public Role? Role { get; set; }
+    }
+}
